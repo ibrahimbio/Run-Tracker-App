6449604b8c7947321025ff508f9b1393080982c7
Ibrahim <ibrahimbioabubakar@gmail.com>
2024-06-07 13:07:46 +0100

    form added to collect running data

--- src/component/RecordRun.js
+++ src/component/RecordRun.js
@@ -1,17 +1,66 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
 
 
 const RecordRun = () => {
 
-    const [date, setDate] = useState('');
+    const [selectedDate, setSelectedDate] = useState('');
     const [distance, setDistance] = useState('');
     const [time, setTime] = useState('');
 
+    //Handler function to update distance state when input changes
+    const handleDistanceChange = (event) => {
+        setDistance(event.target.value)
+    };
+
+    //Handler function to update time state when input changes
+
+    const handleTimeChange = (event) => {
+      setTime(event.target.value)
+    };
+
+    // Handle function to handle form submission
+    const handleSubmit = () => {
+
+      // Prepare the data to be sent in the POST request
+      let data = {
+        "date": selectedDate,
+        "distance": distance,
+        "time": time
+
+      }
+      // Create a new instance of the Request helper
+      const request = new Request();
+      // Send a POST request to the server with the data
+      request.post("http://localhost:9000/api/runs", data)
+      .then(() => {
+
+        // Log the data to the console upon successfull submission
+
+        console.log(data);
+      })
+    }
 
 
   return (
     <div>
-      <p>Record Run Component</p>
+    <h1> Add a new run you absolute champion.</h1>
+    <br />
+    <h2>When did you achieve this incredible feat?</h2>
+    {/* DatePicker component for selecting the date */}
+    < DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)}/>
+    <br />
+    <h2>How many kilometers did you complete like it ain't no thang?</h2>
+    {/* Input field for entering the distance */}
+    <input type="number" id='time' onChange={handleTimeChange} value={time} />
+    <br />
+    <br />
+    <br />
+    {/* Buttion to trigger the form submission  */}
+    <button onClick={handleSubmit}>Save</button>
+
     </div>
   )
 }
